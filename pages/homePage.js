dc6d88ace3cb923e1b03580e7ce76a28e54652d8
tommiecorrigan <tommie-corrigan@hotmail.com>
2026-03-17 15:27:41 -0700

    add platform dropdown tests

--- pages/homePage.js
+++ pages/homePage.js
@@ -3,7 +3,7 @@ class HomePage {
     constructor(page) {
         this.page = page;
         this.header = page.getByRole('banner');
-
+        //header navigation buttons
         this.platformButton = page.getByRole('button', {name: /platform/i});
         this.solutionsButton = page.getByRole('button', {name: /solutions/i});
         this.aboutUsButton = page.getByRole('button', {name: /about us/i});
@@ -11,22 +11,61 @@ class HomePage {
         this.loginButton = page.getByRole('link', {name: /log in/i});
         this.requestDemoButton = this.header.getByRole('link', {name: /request a demo/i});
         this.signupButton = this.header.getByRole('link', {name: /sign up/i});
-        this.stackAdaptMarketingPlatformOption = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /stackadapt marketing platform/i });
+        //platform dropdown options
+        this.stackAdaptMarketingPlatform = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /stackadapt marketing platform/i });
+        this.ivyAiMarketingAssistant = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /ivy™ ai marketing assistant/i });
+        this.partnerProgram = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /partner program/i });
+        this.connectedTv = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /connected tv/i });
+        this.digitalOutOfHome = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /digital out-of-home/i });
+        this.email = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /email/i });
+        this.audio = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /audio/i });
+        this.video = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /video/i });
+        this.native = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /native/i });
+        this.display = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /display/i });
+        this.inGame = page.locator('#submenu-toggle-6-panel').getByRole('link', { name: /in-game/i });
+
 
     }
     async goto() {
-        await this.page.goto('https://www.stackadapt.com/');
+        await this.page.goto('https://stackadapt.com');
     }
-
     async openPlatformDropdown() {
         await this.platformButton.click();
     }
     async clickStackAdaptMarketingPlatformOption() {
-        await this.stackAdaptMarketingPlatformOption.click();
+        await this.stackAdaptMarketingPlatform.click();
     }
-    async verifyStackAdaptMarketingPlatformOptionUrl() {
-        await expect(this.page).toHaveURL(/platform/);
+    async clickIvyAiMarketingAssistantOption() {
+        await this.ivyAiMarketingAssistant.click();
     }
+    async clickPartnerProgramOption() {
+        await this.partnerProgram.click();
+    }
+    async clickConnectedTvOption() {
+        await this.connectedTv.click();
+    }
+    async clickDigitalOutOfHomeOption() {
+        await this.digitalOutOfHome.click();
+    }
+    async clickEmailOption() {
+        await this.email.click();
+    }
+    async clickAudioOption() {
+        await this.audio.click();
+    }
+    async clickVideoOption() {
+        await this.video.click();
+    }
+    async clickNativeOption() {
+        await this.native.click();
+    }
+    async clickDisplayOption() {
+        await this.display.click();
+    }
+    async clickInGameOption() {
+        await this.inGame.click();
+    }
+
 
 }
 module.exports = {HomePage};
--- tests/homePage.test.js
+++ tests/homePage.test.js
@@ -29,11 +29,191 @@ test ('platform dropdown opens and displays options', async ({ page }) => {
         await homePage.openPlatformDropdown();
     });
     await test.step('Verify StackAdapt Marketing Platform option displays', async () => {
-        await expect(homePage.stackAdaptMarketingPlatformOption).toBeVisible();
+        await expect(homePage.stackAdaptMarketingPlatform).toBeVisible();
+    });
+    await test.step('Verify Ivy AI Marketing Assistant option displays', async () => {
+        await expect(homePage.ivyAiMarketingAssistant).toBeVisible();
+    });
+    await test.step('Verify Partner Program option displays', async () => {
+        await expect(homePage.partnerProgram).toBeVisible();
+    });
+    await test.step('Verify Connected TV option displays', async () => {
+        await expect(homePage.connectedTv).toBeVisible();
+    });
+    await test.step('Verify Digital Out-of-Home option displays', async () => {
+        await expect(homePage.digitalOutOfHome).toBeVisible();
+    });
+    await test.step('Verify Email option displays', async () => {
+        await expect(homePage.email).toBeVisible();
+    });
+    await test.step('Verify Audio option displays', async () => {
+        await expect(homePage.audio).toBeVisible();
+    });
+    await test.step('Verify Video option displays', async () => {
+        await expect(homePage.video).toBeVisible();
+    });
+    await test.step('Verify Native option displays', async () => {
+        await expect(homePage.native).toBeVisible();
+    });
+    await test.step('Verify Display option displays', async () => {
+        await expect(homePage.display).toBeVisible();
+    });
+    await test.step('Verify In-Game option displays', async () => {
+        await expect(homePage.inGame).toBeVisible();
+    });
+
+})
+
+test ('click Stack Adapt Marketing Platform option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
     });
     await test.step('Click StackAdapt Marketing Platform option and verify url is accurate', async () => {
         await homePage.clickStackAdaptMarketingPlatformOption();
-        await homePage.verifyStackAdaptMarketingPlatformOptionUrl();
+        await expect(page).toHaveURL(/platform/);
     })
+})
 
+test ('click Ivy AI Marketing Assistant option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Ivy AI Marketing Assistant option and verify url is accurate', async () => {
+        await homePage.clickIvyAiMarketingAssistantOption();
+        await expect(page).toHaveURL(/ivy-ai-marketing-assistant/);
+    })
+})
+
+test ('click Partner Program option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Partner Program option and verify url is accurate', async () => {
+        await homePage.clickPartnerProgramOption();
+        await expect(page).toHaveURL(/partner-program/);
+    });
+})
+
+test ('click Connected TV option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Connected TV option and verify url is accurate', async () => {
+        await homePage.clickConnectedTvOption()
+        await expect(page).toHaveURL(/connected-tv-advertising/);
+    });
+})
+
+test ('click Digital Out-of-Home option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Digital Out-of-Home option and verify url is accurate', async () => {
+        await homePage.clickDigitalOutOfHomeOption()
+        await expect(page).toHaveURL(/digital-out-of-home-advertising/);
+    });
+})
+
+test ('click Email option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Email option and verify url is accurate', async () => {
+        await homePage.clickEmailOption()
+        await expect(page).toHaveURL(/email/);
+    });
+})
+
+test ('click Audio option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Audio option and verify url is accurate', async () => {
+        await homePage.clickAudioOption()
+        await expect(page).toHaveURL(/programmatic-audio-advertising/);
+    });
+})
+
+test.only ('click Video option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Video option and verify url is accurate', async () => {
+        await homePage.clickVideoOption()
+        await expect(page).toHaveURL(/video-advertising/);
+    });
+})
+
+test.only ('click Native option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Native option and verify url is accurate', async () => {
+        await homePage.clickNativeOption()
+        await expect(page).toHaveURL(/native-advertising/);
+    });
+})
+
+test ('click Display option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click Display option and verify url is accurate', async () => {
+        await homePage.clickDisplayOption()
+        await expect(page).toHaveURL(/display-advertising/);
+    });
+})
+
+test ('click In-Game option from platform dropdown and verify url is accurate', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await test.step ('Navigate to homepage', async () => {
+        await homePage.goto()
+    });
+    await test.step('Open platform menu', async () => {
+        await homePage.openPlatformDropdown();
+    });
+    await test.step('Click In-Game option and verify url is accurate', async () => {
+        await homePage.clickInGameOption()
+        await expect(page).toHaveURL(/in-game-advertising/);
+    });
 })
