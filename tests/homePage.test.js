0c9b1e034a71594f78dcf0e0bd078cfb5507ee3b
tommiecorrigan <tommie-corrigan@hotmail.com>
2026-03-17 16:05:56 -0700

    refactor tests to reducec repetitiveness

--- tests/homePage.test.js
+++ tests/homePage.test.js
@@ -64,156 +64,89 @@ test ('platform dropdown opens and displays options', async ({ page }) => {
 
 })
 
-test ('click Stack Adapt Marketing Platform option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
+test.describe.only('Platform dropdown navigation', () => {
+    let homePage;
+
+    test.beforeEach(async ({ page }) => {
+        homePage = new HomePage(page);
+        await homePage.goto();
         await homePage.openPlatformDropdown();
     });
-    await test.step('Click StackAdapt Marketing Platform option and verify url is accurate', async () => {
-        await homePage.clickStackAdaptMarketingPlatformOption();
-        await expect(page).toHaveURL(/platform/);
+
+    test ('click Stack Adapt Marketing Platform option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Stack Adapt Marketing Platform navigation', async () => {
+            await homePage.clickStackAdaptMarketingPlatformOption();
+            await expect(page).toHaveURL(/platform/);
+        })
     })
-})
 
-test ('click Ivy AI Marketing Assistant option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Ivy AI Marketing Assistant option and verify url is accurate', async () => {
-        await homePage.clickIvyAiMarketingAssistantOption();
-        await expect(page).toHaveURL(/ivy-ai-marketing-assistant/);
+    test ('click Ivy AI Marketing Assistant option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Ivy AI Marketing Assistant navigation ', async () => {
+            await homePage.clickIvyAiMarketingAssistantOption();
+            await expect(page).toHaveURL(/ivy-ai-marketing-assistant/);
+        })
     })
-})
 
-test ('click Partner Program option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Partner Program option and verify url is accurate', async () => {
-        await homePage.clickPartnerProgramOption();
-        await expect(page).toHaveURL(/partner-program/);
-    });
-})
+    test ('click Partner Program option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Partner Program navigation', async () => {
+            await homePage.clickPartnerProgramOption();
+            await expect(page).toHaveURL(/partner-program/);
+        });
+    })
 
-test ('click Connected TV option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Connected TV option and verify url is accurate', async () => {
-        await homePage.clickConnectedTvOption()
-        await expect(page).toHaveURL(/connected-tv-advertising/);
-    });
-})
+    test ('click Connected TV option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Connected TV navigation ', async () => {
+            await homePage.clickConnectedTvOption();
+            await expect(page).toHaveURL(/connected-tv-advertising/);
+        });
+    })
 
-test ('click Digital Out-of-Home option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Digital Out-of-Home option and verify url is accurate', async () => {
-        await homePage.clickDigitalOutOfHomeOption()
-        await expect(page).toHaveURL(/digital-out-of-home-advertising/);
-    });
-})
+    test ('click Digital Out-of-Home option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Digital Out-of-Home navigation', async () => {
+            await homePage.clickDigitalOutOfHomeOption();
+            await expect(page).toHaveURL(/digital-out-of-home-advertising/);
+        });
+    })
 
-test ('click Email option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Email option and verify url is accurate', async () => {
-        await homePage.clickEmailOption()
-        await expect(page).toHaveURL(/email/);
-    });
-})
+    test ('click Email option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Email navigation', async () => {
+            await homePage.clickEmailOption();
+            await expect(page).toHaveURL(/email/);
+        });
+    })
 
-test ('click Audio option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Audio option and verify url is accurate', async () => {
-        await homePage.clickAudioOption()
-        await expect(page).toHaveURL(/programmatic-audio-advertising/);
-    });
-})
+    test ('click Audio option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Audio navigation', async () => {
+            await homePage.clickAudioOption();
+            await expect(page).toHaveURL(/programmatic-audio-advertising/);
+        });
+    })
 
-test ('click Video option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Video option and verify url is accurate', async () => {
-        await homePage.clickVideoOption()
-        await expect(page).toHaveURL(/video-advertising/);
-    });
-})
+    test ('click Video option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Video navigation', async () => {
+            await homePage.clickVideoOption();
+            await expect(page).toHaveURL(/video-advertising/);
+        });
+    })
 
-test ('click Native option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Native option and verify url is accurate', async () => {
-        await homePage.clickNativeOption()
-        await expect(page).toHaveURL(/native-advertising/);
-    });
-})
+    test ('click Native option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Native navigation', async () => {
+            await homePage.clickNativeOption();
+            await expect(page).toHaveURL(/native-advertising/);
+        });
+    })
 
-test ('click Display option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click Display option and verify url is accurate', async () => {
-        await homePage.clickDisplayOption()
-        await expect(page).toHaveURL(/display-advertising/);
-    });
-})
+    test ('click Display option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify Display navigation', async () => {
+            await homePage.clickDisplayOption();
+            await expect(page).toHaveURL(/display-advertising/);
+        });
+    })
 
-test ('click In-Game option from platform dropdown and verify url is accurate', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await test.step ('Navigate to homepage', async () => {
-        await homePage.goto()
-    });
-    await test.step('Open platform menu', async () => {
-        await homePage.openPlatformDropdown();
-    });
-    await test.step('Click In-Game option and verify url is accurate', async () => {
-        await homePage.clickInGameOption()
-        await expect(page).toHaveURL(/in-game-advertising/);
-    });
+    test ('click In-Game option from platform dropdown and verify url is accurate', async ({ page }) => {
+        await test.step('verify In-Game navigation', async () => {
+            await homePage.clickInGameOption();
+            await expect(page).toHaveURL(/in-game-advertising/);
+        });
+    })
 })
